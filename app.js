172c4cdf39f436b7e57d0c06d07ed5172520231f
Destoky <adestoky@gmail.com>
2020-12-12 20:49:37 +0100

    Choix du nombre
    input pour que l'utilisateur choisisse le nombre d'éléments a vérifier

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+//recherche du dom
 const box = document.querySelector(".container");
+const nombre = document.querySelector(".nombre");
+const btn = document.querySelector(".bouton");
+const limite = document.querySelector(".limite");
+
+//variables de travail
+let nombreChoisi;
+
+btn.addEventListener("click", () => {
+  if (nombre.value == "") {
+    alert("veuillez entrez un nombre");
+  } else {
+    nombreChoisi = nombre.value;
+    limite.style.display = "none";
+    affichageNombre(nombreChoisi);
+  }
+});
+
 
 //Fonction de vérification de nombre premier
 let verifPremier = (nombreATester) => {
@@ -12,21 +30,22 @@ let verifPremier = (nombreATester) => {
 };
 
 
+//fonction d'affichage des nombres
+let affichageNombre = (nombreChoisi) => {
+  for (let i = 1; i <= nombreChoisi; i++) {
+    let div = document.createElement("span");
+    div.textContent = i;
+    box.append(div);
 
+    //Retour a la ligne a chaque dizaine
+    if (i % 10 == 0) {
+      div.style.display = "inline-block";
+    }
 
-for (let i = 1; i <= 500; i++) {
-  let div = document.createElement("span");
-  div.textContent = i;
-  box.append(div);
-
-  //Retour a la ligne a chaque dizaine
-  if (i % 10 == 0) {
-    div.style.display = "inline-block";
+    //si le nombre est premier on change sa couleur
+    if (verifPremier(i)) {
+      div.style.background = "rgb(15, 100, 173)";
+      div.style.color = "rgb(240, 155, 80)";
+    }
   }
-
-  //si le nombre est premier on change sa couleur
-  if(verifPremier(i)) {
-    div.style.background = "rgb(58, 7, 122)";
-    div.style.color = "rgb(190, 176, 132)";
-  }
-}
+};
